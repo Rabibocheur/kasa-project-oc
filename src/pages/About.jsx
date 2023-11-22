1e36d05a70fe3c94f681248ec956b84a29bfa7ef
Rabibocheur <beaupuyg@outlook.fr>
2023-11-22 12:08:45 +0100

    add of the 4 reductions of the about page01~

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,20 +6,36 @@ function About() {
   return (
     <div>
       <Banner image={aboutBanner} altImg="about banner" />
-      <Collapse title="test" content="test bnojour comment ca va " />
       <Collapse
-        title="test"
-        content={[
-          "Climatisation",
-          "Wi-Fi",
-          "Cuisine",
-          "Espace de travail",
-          "Fer à repasser",
-          "Sèche-ch",
-        ]}
-        type="list"
+        title="Fiabilité"
+        content={`
+        Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+        régulièrement vérifiées par nos équipes.`}
+        type="text"
       />
-      <Collapse title="test" content="test" />
+      <Collapse
+        title="Respect"
+        content={`
+        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+        perturbation du voisinage entraînera une exclusion de notre plateforme.`}
+        type="text"
+      />
+      <Collapse
+        title="Service"
+        content={`
+        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+        perturbation du voisinage entraînera une exclusion de notre plateforme.`}
+        type="text"
+      />
+      <Collapse
+        title="Sécurité"
+        content={`
+        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+        correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+        locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+        également des ateliers sur la sécurité domestique pour nos hôtes.`}
+        type="text"
+      />
     </div>
   );
 }
